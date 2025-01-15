7e99b00319432f9229626c2766009d87989065b2
bwroc <bw19xp@brocku.ca>
2025-01-15 15:17:34 -0500

    Added API details (Benjamin Williams)

--- code/server/app/src/main/java/server/web/API.java
+++ code/server/app/src/main/java/server/web/API.java
@@ -1,4 +1,27 @@
 package server.web;
 
+import server.db.Transaction;
+import server.web.annotations.Json;
+import server.web.annotations.Route;
+import server.web.annotations.url.Path;
+import util.SqlSerde;
+
+import java.sql.SQLException;
+
+
+
 public class API {
+
+    public static final class Person {
+        public int id;
+        public String name;
+    }
+
+    @Route("/person/<id>")
+    public static @Json Person person(Transaction trans, @Path int id) throws SQLException {
+        try (var stmt = trans.conn.namedPreparedStatement("select * from person where id=:id")){
+            stmt.setInt(":id",id);
+            return SqlSerde.sqlSingle(stmt.executeQuery(), Person.class);
+        }
+    }
 }
