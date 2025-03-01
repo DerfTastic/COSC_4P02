7911948b2cf4634f8807c996e27d2ca1b006a420
ParkerTenBroeck <51721964+ParkerTenBroeck@users.noreply.github.com>
2025-03-01 18:04:20 -0500

    cleaned internal code

--- code/server/app/src/main/java/framework/web/route/RequestsBuilder.java
+++ code/server/app/src/main/java/framework/web/route/RequestsBuilder.java
@@ -3,6 +3,7 @@ package framework.web.route;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.sun.net.httpserver.HttpExchange;
+import framework.util.func.Function1;
 import framework.web.annotations.Body;
 import framework.web.annotations.FromRequest;
 import framework.web.annotations.Json;
@@ -16,6 +17,7 @@ import framework.util.TypeReflect;
 
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
+import java.util.Map;
 
 public class RequestsBuilder {
 
@@ -76,123 +78,82 @@ public class RequestsBuilder {
     }
 
     private StringsAdapter<?> getParameterStringAdapter(Class<?> type, boolean nullable) {
+        if(type.isPrimitive()&&nullable) throw new RuntimeException("Cannot be nullable primitive");
         if(type.isPrimitive()) return getParameterStringAdapter(TypeReflect.wrapPrimitives(type), nullable);
 
-        if (type.equals(Integer.class)) {
-            if(nullable)
-                return (StringSingleNullableAdapter<?>)Integer::parseInt;
+        Map<Class<?>, Function1<Object, String>> map = Map.of(
+                Byte.class, Byte::parseByte,
+                Short.class, Short::parseShort,
+                Integer.class, Integer::parseInt,
+                Long.class, Long::parseLong,
+                Boolean.class, Boolean::parseBoolean,
+                Float.class, Float::parseFloat,
+                Double.class, Double::parseDouble,
+                String.class, s -> s
+        );
+        if(map.containsKey(type)){
+            if (nullable)
+                return (StringSingleNullableAdapter<?>)map.get(type)::call;
             else
-                return (StringSingleAdapter<?>)Integer::parseInt;
-        } else if (type.equals(Short.class)) {
-            if(nullable)
-                return (StringSingleNullableAdapter<?>)Short::parseShort;
-            else
-                return (StringSingleAdapter<?>)Short::parseShort;
-        } else if (type.equals(Byte.class)) {
-            if(nullable)
-                return (StringSingleNullableAdapter<?>)Byte::parseByte;
-            else
-                return (StringSingleAdapter<?>)Byte::parseByte;
-        } else if (type.equals(Long.class)) {
-            if(nullable)
-                return (StringSingleNullableAdapter<?>)Long::parseLong;
-            else
-                return (StringSingleAdapter<?>)Long::parseLong;
-        } else if (type.equals(Boolean.class)) {
-            if(nullable)
-                return (StringSingleNullableAdapter<?>)Boolean::parseBoolean;
-            else
-                return (StringSingleAdapter<?>)Boolean::parseBoolean;
-        } else if (type.equals(Float.class)) {
-            if(nullable)
-                return (StringSingleNullableAdapter<?>)Float::parseFloat;
-            else
-                return (StringSingleAdapter<?>)Float::parseFloat;
-        } else if (type.equals(Double.class)) {
-            if(nullable)
-                return (StringSingleNullableAdapter<?>)Double::parseDouble;
-            else
-                return (StringSingleAdapter<?>)Double::parseDouble;
-        } else if (type.equals(String.class)) {
-            if(nullable)
-                return (StringSingleNullableAdapter<?>) s->s;
-            else
-                return (StringSingleAdapter<?>) s->s;
+                return (StringSingleAdapter<?>)map.get(type)::call;
         }
-        if (type.equals(int[].class)) {
-            var sa = getParameterStringAdapter(type.getComponentType(), nullable);
-            return (StringListAdapter<?>) list -> {
-                if(nullable&&list==null)return null;
-                var l = new int[list.size()];
-                for (int i = 0; i < l.length; i ++)
-                    l[i] = (int) sa.parseSingular(list.get(i));
-                return l;
-            };
-        } else if (type.equals(short[].class)) {
-            var sa = getParameterStringAdapter(type.getComponentType(), nullable);
-            return (StringListAdapter<?>) list -> {
-                if(nullable&&list==null)return null;
-                var l = new short[list.size()];
-                for (int i = 0; i < l.length; i ++)
-                    l[i] = (short) sa.parseSingular(list.get(i));
-                return l;
-            };
-        } else if (type.equals(byte[].class)) {
-            var sa = getParameterStringAdapter(type.getComponentType(), nullable);
-            return (StringListAdapter<?>) list -> {
-                if(nullable&&list==null)return null;
-                var l = new short[list.size()];
-                for (int i = 0; i < l.length; i ++)
-                    l[i] = (short) sa.parseSingular(list.get(i));
-                return l;
-            };
-        } else if (type.equals(long[].class)) {
-            var sa = getParameterStringAdapter(type.getComponentType(), nullable);
-            return (StringListAdapter<?>) list -> {
-                if(nullable&&list==null)return null;
-                var l = new long[list.size()];
-                for (int i = 0; i < l.length; i ++)
-                    l[i] = (long) sa.parseSingular(list.get(i));
-                return l;
-            };
-        } else if (type.equals(boolean[].class)) {
-            var sa = getParameterStringAdapter(type.getComponentType(), nullable);
-            return (StringListAdapter<?>) list -> {
-                if(nullable&&list==null)return null;
-                var l = new boolean[list.size()];
-                for (int i = 0; i < l.length; i ++)
-                    l[i] = (boolean) sa.parseSingular(list.get(i));
-                return l;
-            };
-        } else if (type.equals(float[].class)) {
-            var sa = getParameterStringAdapter(type.getComponentType(), nullable);
-            return (StringListAdapter<?>) list -> {
-                if(nullable&&list==null)return null;
-                var l = new float[list.size()];
-                for (int i = 0; i < l.length; i ++)
-                    l[i] = (float) sa.parseSingular(list.get(i));
-                return l;
-            };
-        } else if (type.equals(double[].class)) {
-            var sa = getParameterStringAdapter(type.getComponentType(), nullable);
-            return (StringListAdapter<?>) list -> {
-                if(nullable&&list==null)return null;
-                var l = new float[list.size()];
-                for (int i = 0; i < l.length; i ++)
-                    l[i] = (float) sa.parseSingular(list.get(i));
-                return l;
-            };
-        } else if (type.isArray()) {
-            if (!type.getComponentType().isArray()) {
-                var sa = getParameterStringAdapter(type.getComponentType(), nullable);
-                return (StringListAdapter<?>) list -> {
+
+        if (type.isArray()) {
+            var innerType = type.getComponentType();
+            boolean innerNullable = innerType.isPrimitive()?false:nullable;
+            var sa = getParameterStringAdapter(innerType, innerNullable);
+
+            return switch(innerType){
+                case Class<?> cl when cl == byte.class -> (StringListAdapter<?>) list -> {
+                    if(nullable&&list==null)return null;
+                    var l = new short[list.size()];
+                    for (int i = 0; i < l.length; i ++)
+                        l[i] = (short) sa.parseSingular(list.get(i));
+                    return l;
+                };
+                case Class<?> cl when cl == short.class -> (StringListAdapter<?>) list -> {
+                    if(nullable&&list==null)return null;
+                    var l = new short[list.size()];
+                    for (int i = 0; i < l.length; i ++)
+                        l[i] = (short) sa.parseSingular(list.get(i));
+                    return l;
+                };
+                case Class<?> cl when cl == int.class -> (StringListAdapter<?>) list -> {
+                    if(nullable&&list==null)return null;
+                    var l = new int[list.size()];
+                    for (int i = 0; i < l.length; i ++)
+                        l[i] = (int) sa.parseSingular(list.get(i));
+                    return l;
+                };
+                case Class<?> cl when cl == long.class -> (StringListAdapter<?>) list -> {
+                    if(nullable&&list==null)return null;
+                    var l = new long[list.size()];
+                    for (int i = 0; i < l.length; i ++)
+                        l[i] = (long) sa.parseSingular(list.get(i));
+                    return l;
+                };
+                case Class<?> cl when cl == float.class -> (StringListAdapter<?>) list -> {
+                    if(nullable&&list==null)return null;
+                    var l = new float[list.size()];
+                    for (int i = 0; i < l.length; i ++)
+                        l[i] = (float) sa.parseSingular(list.get(i));
+                    return l;
+                };
+                case Class<?> cl when cl == double.class -> (StringListAdapter<?>) list -> {
+                    if(nullable&&list==null)return null;
+                    var l = new float[list.size()];
+                    for (int i = 0; i < l.length; i ++)
+                        l[i] = (float) sa.parseSingular(list.get(i));
+                    return l;
+                };
+                default -> (StringListAdapter<?>) list -> {
                     if(nullable&&list==null)return null;
                     var l = new Object[list.size()];
                     for (int i = 0; i < l.length; i ++)
                         l[i] = sa.parseSingular(list.get(i));
                     return l;
                 };
-            }
+            };
         }
         throw new RuntimeException("Cannot get String handler for " + type);
     }
