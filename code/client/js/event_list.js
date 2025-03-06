8c8c9dc197e30e8380504b1e388738a0c39675e8
Taylor <tchap28@hotmail.com>
2025-03-06 10:57:34 -0500

    Update event_list.js

--- code/client/js/event_list.js
+++ code/client/js/event_list.js
@@ -25,7 +25,7 @@ document.addEventListener("DOMContentLoaded", function () {
         eventList.forEach(event => {
             const ticketSVG = document.createElement("div");
             ticketSVG.innerHTML = `
-                <svg width="600" height="300" viewBox="0 0 400 200" xmlns="http://www.w3.org/2000/svg">
+                <svg width="800" height="400" viewBox="0 0 400 200" xmlns="http://www.w3.org/2000/svg">
                     <path d="M0,40 Q20,40 20,20 H380 Q380,40 400,40 V80 Q380,80 380,100 Q380,120 400,120 V160 Q380,160 380,180 H20 Q20,160 0,160 V120 Q20,120 20,100 Q20,80 0,80 Z" 
                           fill="white" stroke="#ccc" stroke-width="2"/>
                     <text x="40" y="60" font-size="22" font-family="Arial, sans-serif" font-weight="bold" fill="#333">${event.title}</text>
