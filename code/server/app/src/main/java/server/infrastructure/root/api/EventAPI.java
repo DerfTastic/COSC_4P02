e7f7e40444dbc1ba0a443055c4a1043aff7ce93f
ParkerTenBroeck <51721964+ParkerTenBroeck@users.noreply.github.com>
2025-03-11 22:50:28 -0400

    updated how event updates work

--- code/server/app/src/main/java/server/infrastructure/root/api/EventAPI.java
+++ code/server/app/src/main/java/server/infrastructure/root/api/EventAPI.java
@@ -1,8 +1,11 @@
 package server.infrastructure.root.api;
 
 import com.alibaba.fastjson2.JSONObject;
+import com.alibaba.fastjson2.JSONReader;
 import framework.web.annotations.*;
 import framework.web.error.BadRequest;
+import framework.web.request.Request;
+import framework.web.route.RouteParameter;
 import server.infrastructure.DynamicMediaHandler;
 import framework.db.RoTransaction;
 import framework.db.RwTransaction;
@@ -14,6 +17,7 @@ import framework.util.SqlSerde;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Optional;
 
 @SuppressWarnings("unused")
 @Routes
@@ -88,51 +92,166 @@ public class EventAPI {
         return new AllEvent(event, tags);
     }
 
-    public record UpdateEvent(
-            long id,
-            String name,
-            String description,
-            JSONObject metadata,
+    @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
+    public static class UpdateEvent{
 
-            String type,
-            String category,
+        long id;
+        Optional<String> name;
+        Optional<String> description;
+        Optional<JSONObject> metadata;
 
-            Long start,
-            Long duration,
+        Optional<String> type;
+        Optional<String> category;
 
-            Integer available_total_tickets,
+        Optional<Long> start;
+        Optional<Long> duration;
 
-            String location_name,
-            Double location_lat,
-            Double location_long
-    ){}
+        Optional<Long> available_total_tickets;
 
+        Optional<String> location_name;
+        Optional<Double> location_lat;
+        Optional<Double> location_long;
+
+        private static long requireInt64(JSONReader reader){
+            if(reader.isNumber())
+                return reader.readInt64Value();
+            else
+                throw new RuntimeException("Expected int64 value");
+        }
+
+        private static Optional<String> optionalString(JSONReader reader){
+            if(reader.nextIfNull())
+                return Optional.empty();
+            if(reader.isString())
+                return Optional.of(reader.readString());
+            else
+                throw new RuntimeException("Expected String value");
+        }
+
+        private static Optional<Long> optionalLong(JSONReader reader){
+            if(reader.nextIfNull())
+                return Optional.empty();
+            if(reader.isNumber())
+                return Optional.of(reader.readInt64());
+            else
+                throw new RuntimeException("Expected int64 value");
+        }
+
+        private static Optional<Double> optionalDouble(JSONReader reader){
+            if(reader.nextIfNull())
+                return Optional.empty();
+            if(reader.isNumber())
+                return Optional.of(reader.readDouble());
+            else
+                throw new RuntimeException("Expected double value");
+        }
+
+        private static Optional<JSONObject> optionalObject(JSONReader reader){
+            if(reader.nextIfNull())
+                return Optional.empty();
+            if(reader.isObject())
+                return Optional.of(reader.readJSONObject());
+            else
+                throw new RuntimeException("Expected object value");
+        }
+
+        public UpdateEvent(JSONReader reader) throws BadRequest {
+            if(!reader.nextIfObjectStart())
+                throw new BadRequest("Expected an object");
+            while(!reader.nextIfObjectEnd()){
+                if(!reader.isString())
+                    throw new BadRequest("Expected field");
+                var field_name = reader.readString();
+                if(!reader.nextIfMatch(':'))
+                    throw new BadRequest("Expected colon");
+                switch(field_name){
+                    case "id" -> this.id = requireInt64(reader);
+                    case "name" -> this.name = optionalString(reader);
+                    case "description" -> this.description = optionalString(reader);
+                    case "metadata" -> this.metadata = optionalObject(reader);
+                    case "type" -> this.type = optionalString(reader);
+                    case "category" -> this.category = optionalString(reader);
+                    case "start" -> this.start = optionalLong(reader);
+                    case "duration" -> this.duration = optionalLong(reader);
+                    case "available_total_tickets" -> this.available_total_tickets = optionalLong(reader);
+                    case "location_name" -> this.location_name = optionalString(reader);
+                    case "location_lat" -> this.location_lat = optionalDouble(reader);
+                    case "location_long" -> this.location_long = optionalDouble(reader);
+                    default -> throw new BadRequest("Unknown field: " + field_name);
+                }
+            }
+        }
+    }
+
+    public static class UpdateEventFromRequest implements RouteParameter<UpdateEvent>{
+        @Override
+        public UpdateEvent construct(Request request) throws Exception {
+            try(var reader = JSONReader.of(request.exchange.getRequestBody().readAllBytes())){
+                return new UpdateEvent(reader);
+            }
+        }
+    }
+
+
+    @SuppressWarnings("OptionalAssignedToNull")
     @Route
-    public static void update_event(@FromRequest(RequireOrganizer.class)UserSession session, RwTransaction trans, @Body @Json UpdateEvent update) throws SQLException, BadRequest {
-        try(var stmt = trans.namedPreparedStatement("update events set name=:name, description=:description, type=:type, category=:category, start=:start, duration=:duration, metadata=:metadata, available_total_tickets=:available_total_tickets, location_name=:location_name, location_lat=:location_lat, location_long=:location_long where id=:id AND owner_id=:owner_id")){
+    public static void update_event(@FromRequest(RequireOrganizer.class)UserSession session, RwTransaction trans, @FromRequest(UpdateEventFromRequest.class) UpdateEvent update) throws SQLException, BadRequest {
+        var str = new StringBuilder().append("update events set ");
+
+        if(update.name!=null)
+            str.append("name=:name,");
+        if(update.description!=null)
+            str.append("description=:description,");
+        if(update.category!=null)
+            str.append("category=:category,");
+        if(update.type!=null)
+            str.append("type=:type,");
+        if(update.start!=null)
+            str.append("start=:start,");
+        if(update.duration!=null)
+            str.append("duration=:duration,");
+        if(update.metadata!=null)
+            str.append("metadata=:metadata,");
+        if(update.available_total_tickets!=null)
+            str.append("available_total_tickets=:available_total_tickets,");
+        if(update.location_name!=null)
+            str.append("location_name=:location_name,");
+        if(update.location_lat!=null)
+            str.append("location_lat=:location_lat,");
+        if(update.location_long!=null)
+            str.append("location_long=:location_long,");
+
+        if(str.charAt(str.length()-1)!=',') return;
+        str.deleteCharAt(str.length()-1);
+        str.append(" where id=:id AND owner_id=:owner_id");
+
+        try(var stmt = trans.namedPreparedStatement(str.toString())){
             stmt.setLong(":id", update.id);
             stmt.setLong(":owner_id", session.user_id);
 
-            stmt.setString(":name", update.name);
-            stmt.setString(":description", update.description);
-            if(update.category!=null)
-                stmt.setString(":category", update.category);
-            if(update.type!=null)
-                stmt.setString(":type", update.type);
-            if(update.start!=null)
-                stmt.setLong(":start", update.start);
-            if(update.duration!=null)
-                stmt.setLong(":duration", update.duration);
-            if(update.metadata!=null)
-                stmt.setString(":metadata", update.metadata.toJSONString());
-            if(update.available_total_tickets!=null)
-                stmt.setInt(":available_total_tickets", update.available_total_tickets);
-            if(update.location_name!=null)
-                stmt.setString(":location_name", update.location_name);
-            if(update.location_lat!=null)
-                stmt.setDouble(":location_lat", update.location_lat);
-            if(update.location_long!=null)
-                stmt.setDouble(":location_long", update.location_long);
+            if(update.name!=null&&update.name.isPresent())
+                stmt.setString(":name", update.name.get());
+            if(update.description!=null&&update.description.isPresent())
+                stmt.setString(":description", update.description.get());
+            if(update.category!=null&&update.category.isPresent())
+                stmt.setString(":category", update.category.get());
+            if(update.type!=null&&update.type.isPresent())
+                stmt.setString(":type", update.type.get());
+            if(update.start!=null&&update.start.isPresent())
+                stmt.setLong(":start", update.start.get());
+            if(update.duration!=null&&update.duration.isPresent())
+                stmt.setLong(":duration", update.duration.get());
+            if(update.metadata!=null&&update.metadata.isPresent())
+                stmt.setString(":metadata", update.metadata.get().toJSONString());
+            if(update.available_total_tickets!=null&&update.available_total_tickets.isPresent())
+                stmt.setLong(":available_total_tickets", update.available_total_tickets.get());
+            if(update.location_name!=null&&update.location_name.isPresent())
+                stmt.setString(":location_name", update.location_name.get());
+            if(update.location_lat!=null&&update.location_lat.isPresent())
+                stmt.setDouble(":location_lat", update.location_lat.get());
+            if(update.location_long!=null&&update.location_long.isPresent())
+                stmt.setDouble(":location_long", update.location_long.get());
+
             if(stmt.executeUpdate()!=1)
                 throw new BadRequest("Failed to update event");
         }
