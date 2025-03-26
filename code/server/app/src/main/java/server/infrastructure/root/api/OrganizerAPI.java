c9887e300360015bda0025275004980bfbd75ae8
Parker TenBroeck <51721964+ParkerTenBroeck@users.noreply.github.com>
2025-03-25 23:30:34 -0400

    added ticket verification API

--- code/server/app/src/main/java/server/infrastructure/root/api/OrganizerAPI.java
+++ code/server/app/src/main/java/server/infrastructure/root/api/OrganizerAPI.java
@@ -26,10 +26,29 @@ public class OrganizerAPI {
     ){}
 
     @Route
-    public static @Json List<Scan> scan_ticket(@FromRequest(RequireOrganizer.class)UserSession auth, RwTransaction trans, @Body@Json Scan scan) throws SQLException {
-        try(var stmt = trans.namedPreparedStatement("insert into ")){
-
+    public static @Json List<PreviousScan> scan_ticket(@FromRequest(RequireOrganizer.class)UserSession auth, RwTransaction trans, @Body@Json Scan scan) throws SQLException, BadRequest {
+        try(var stmt = trans.namedPreparedStatement("select coalesce((select ticket_id from purchased_tickets where id=:purchased_ticket_id AND salt=:salt) IN (select id from tickets where event_id=:event_id AND owner_id=:user_id),false)")){
+            stmt.setLong(":purchased_ticket_id", scan.id.id());
+            stmt.setString(":salt", scan.id.salt());
+            stmt.setLong(":event_id", scan.event);
+            stmt.setLong(":user_id", auth.user_id);
+            if(!SqlSerde.sqlSingle(stmt.executeQuery(), rs -> rs.getBoolean(1)))
+                throw new BadRequest();
         }
-        return null;
+
+        try(var stmt = trans.namedPreparedStatement("insert into scanned_tickets values(:purchased_ticket, :time_scanned)")){
+            stmt.setLong(":purchased_ticket", scan.id.id());
+            stmt.setLong(":time_scanned", System.currentTimeMillis());
+            stmt.execute();
+        }
+
+        List<PreviousScan> scans;
+        try(var stmt = trans.namedPreparedStatement("select time_scanned from scanned_tickets where purchased_ticket=:purchased_ticket")){
+            stmt.setLong(":purchased_ticket", scan.id.id());
+            scans = SqlSerde.sqlList(stmt.executeQuery(), rs -> new PreviousScan(rs.getLong(1)));
+        }
+
+        trans.commit();
+        return scans;
     }
 }
